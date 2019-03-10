5b756b40fea7816cdbe660032f64f43dd1b5b5d8
fcorow <fcorvaisier@gmail.com>
2019-03-10 16:51:54 +0100

    try exo list

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -1,4 +1,5 @@
 defmodule ListOps do
+  require Logger
   # Please don't use any external modules (especially List or Enum) in your
   # implementation. The point of this exercise is to create these basic
   # functions yourself. You may use basic Kernel functions (like `Kernel.+/2`
@@ -6,24 +7,50 @@ defmodule ListOps do
   # `++`, `--`, `hd`, `tl`, `in`, and `length`.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-  end
+
+  def count([]), do: 0
+
+  def count([_head | tail]), do: 1 + count(tail)
 
   @spec reverse(list) :: list
-  def reverse(l) do
+  def reverse([]), do: []
+  # def reverse([_a]), do: [_a]
+  # def reverse([a, b]), do: [b, a]
+  def reverse([head | []]) do
+    Logger.info("head_empty")
+    [head]
+  end
+
+  def reverse([head | tail]) do
+    Logger.info("head_tail")
+    reverse(tail)
   end
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-  end
+  def map([], f), do: []
+  def map([head | tail], f), do: [f.(head) | map(tail, f)]
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
+  def filter([], f) do
+    []
+  end
+
+  def filter([head | tail], f) do
+    if f.(head) do
+      [head | filter(tail, f)]
+    else
+      filter(tail, f)
+    end
   end
 
   @type acc :: any
   @spec reduce(list, acc, (any, acc -> acc)) :: acc
-  def reduce(l, acc, f) do
+  def reduce([], acc, f) do
+    acc
+  end
+
+  def reduce([head | tail], acc, f) do
+    reduce(tail, f.(head, acc), f)
   end
 
   @spec append(list, list) :: list
@@ -34,3 +61,6 @@ defmodule ListOps do
   def concat(ll) do
   end
 end
+
+list = [1, 2, 3]
+IO.inspect(ListOps.reverse(list))
--- elixir/list-ops/list_ops_test.exs
+++ elixir/list-ops/list_ops_test.exs
@@ -17,17 +17,17 @@ defmodule ListOpsTest do
     assert L.count([]) == 0
   end
 
-  @tag :pending
+  # @tag :pending
   test "count of normal list" do
     assert L.count([1, 3, 5, 7]) == 4
   end
 
-  @tag :pending
+  # @tag :pending
   test "count of huge list" do
     assert L.count(Enum.to_list(1..1_000_000)) == 1_000_000
   end
 
-  @tag :pending
+  # @tag :pending
   test "reverse of empty list" do
     assert L.reverse([]) == []
   end
@@ -42,47 +42,47 @@ defmodule ListOpsTest do
     assert L.reverse(Enum.to_list(1..1_000_000)) == Enum.to_list(1_000_000..1)
   end
 
-  @tag :pending
+  # @tag :pending
   test "map of empty list" do
     assert L.map([], &(&1 + 1)) == []
   end
 
-  @tag :pending
+  # @tag :pending
   test "map of normal list" do
     assert L.map([1, 3, 5, 7], &(&1 + 1)) == [2, 4, 6, 8]
   end
 
-  @tag :pending
+  # @tag :pending
   test "map of huge list" do
     assert L.map(Enum.to_list(1..1_000_000), &(&1 + 1)) == Enum.to_list(2..1_000_001)
   end
 
-  @tag :pending
+  # @tag :pending
   test "filter of empty list" do
     assert L.filter([], &odd?/1) == []
   end
 
-  @tag :pending
+  # @tag :pending
   test "filter of normal list" do
     assert L.filter([1, 2, 3, 4], &odd?/1) == [1, 3]
   end
 
-  @tag :pending
+  # @tag :pending
   test "filter of huge list" do
     assert L.filter(Enum.to_list(1..1_000_000), &odd?/1) == Enum.map(1..500_000, &(&1 * 2 - 1))
   end
 
-  @tag :pending
+  # @tag :pending
   test "reduce of empty list" do
     assert L.reduce([], 0, &(&1 + &2)) == 0
   end
 
-  @tag :pending
+  # @tag :pending
   test "reduce of normal list" do
     assert L.reduce([1, 2, 3, 4], -3, &(&1 + &2)) == 7
   end
 
-  @tag :pending
+  # @tag :pending
   test "reduce of huge list" do
     assert L.reduce(Enum.to_list(1..1_000_000), 0, &(&1 + &2)) ==
              Enum.reduce(1..1_000_000, 0, &(&1 + &2))
